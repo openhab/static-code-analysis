07295de0857922093a055b5e728593d70e4fc405
Mihaela K. Memova <mihaela.memova@MUSALA.COM>
2017-04-20 17:20:32 +0300

    A check for importing all exported packages + tests for it.
    Signed-off-by: Mihaela Memova <mihaela.memova@musala.com>

---
+++ src/main/java/org/openhab/tools/analysis/checkstyle/ImportExportedPackagesCheck.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.tools.analysis.checkstyle;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.ivy.osgi.core.BundleRequirement;
+import org.apache.ivy.osgi.core.ExportPackage;
+import org.apache.ivy.osgi.core.ManifestParser;
+import org.openhab.tools.analysis.checkstyle.api.AbstractStaticCheck;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
+
+/**
+ * Checks if all of the exported packages are imported by the bundle itself
+ *
+ * @author Mihaela Memova
+ */
+public class ImportExportedPackagesCheck extends AbstractStaticCheck {
+
+    public static final String MANIFEST_EXTENSTION = "MF";
+    public static final String EXPORT_PACKAGES_HEADER = "Export-Package:";
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public ImportExportedPackagesCheck() {
+        setFileExtensions(MANIFEST_EXTENSTION);
+    }
+
+    @Override
+    protected void processFiltered(File file, List<String> lines) throws CheckstyleException {
+        int lineNumberBeforeExportsStart = findLineNumber(lines, EXPORT_PACKAGES_HEADER, 0) - 1;
+
+        try {
+            Set<ExportPackage> exports = ManifestParser.parseManifest(file).getExports();
+            Set<BundleRequirement> imports = ManifestParser.parseManifest(file).getImports();
+
+            for (ExportPackage export : exports) {
+                if (!isPackageImported(imports, export)) {
+                    int lineToLog = findLineNumber(lines, export.toString(), lineNumberBeforeExportsStart);
+                    log(lineToLog, String.format("The exported package: %s is not imported", export));
+                }
+            }
+        } catch (IOException e) {
+            logger.error("An error occured while processing the file {}", file.getPath(), e);
+        } catch (ParseException e) {
+            logger.error("An error occured while trying to parse the MANIFEST {}", file.getPath(), e);
+        }
+    }
+
+    /**
+     * Checks if a particular package is imported. The default implementation of
+     * {@link Set#contains(Object)} cannot be used since an {@link ExportPackage} object
+     * is searched in a set of {link {@link BundleRequirement} objects.
+     *
+     * @param imports - set of all imported packages
+     * @param searchedPackage - the package that has to be checked
+     * @return true if the imports contains the package, false otherwise
+     */
+    private boolean isPackageImported(Set<BundleRequirement> imports, ExportPackage searchedPackage) {
+        for (Object o : imports) {
+            if ((o.toString()).equals(searchedPackage.toString())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/test/java/org/openhab/tools/analysis/checkstyle/test/ImportExportedPackagesCheckTest.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.tools.analysis.checkstyle.test;
+
+import java.io.File;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openhab.tools.analysis.checkstyle.ImportExportedPackagesCheck;
+import org.openhab.tools.analysis.checkstyle.api.AbstractStaticCheckTest;
+
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import com.puppycrawl.tools.checkstyle.api.Configuration;
+import com.puppycrawl.tools.checkstyle.utils.CommonUtils;
+
+/**
+ * Tests for {@link ImportExportedPackagesCheck}
+ *
+ * @author Mihaela Memova
+ *
+ */
+public class ImportExportedPackagesCheckTest extends AbstractStaticCheckTest {
+
+    private static DefaultConfiguration configuration;
+
+    @BeforeClass
+    public static void setUp() {
+        configuration = createCheckConfig(ImportExportedPackagesCheck.class);
+    }
+
+    private static final String TEST_DIRECTORY_NAME = "importExportedPackagesCheckTest";
+    private static final String NOT_IMPORTED_PACKAGE_MESSAGE = "The exported package is not imported";
+
+    @Test
+    public void testManifestFileThatDoesNotImportAnExportedPackage() throws Exception {
+        String testFileName = "ManifestNotImportingAnExportedPackage.MF";
+        int lineNumber = 12;
+        String[] warningMessages = generateExpectedMessages(lineNumber, NOT_IMPORTED_PACKAGE_MESSAGE);
+        verifyManifestFile(testFileName, warningMessages);
+    }
+
+    @Test
+    public void testManifestFileThatDoesNotImportSeveralExportedPackages() throws Exception {
+
+        String testFileName = "ManifestNotImportingSeveralExportedPackages.MF";
+        int firstLineNumber = 14;
+        int secondLineNumber = 15;
+
+        String[] warningMessages = generateExpectedMessages(firstLineNumber, NOT_IMPORTED_PACKAGE_MESSAGE,
+                secondLineNumber, NOT_IMPORTED_PACKAGE_MESSAGE);
+        verifyManifestFile(testFileName, warningMessages);
+    }
+
+    @Test
+    public void testManifestFileThatDoesNotImportAnyPackages() throws Exception {
+
+        String testFileName = "ManifestNotImportingAnyPackages.MF";
+        int lineNumber = 10;
+
+        String[] warningMessages = generateExpectedMessages(lineNumber,
+                ImportExportedPackagesCheck.NOT_IMPORTED_PACKAGE_MESSAGE);
+        verifyManifestFile(testFileName, warningMessages);
+    }
+
+    @Test
+    public void testManifestThatImportAllExportedPackages() throws Exception {
+
+        String testFileName = "ManifestImportingAllExportedPackages.MF";
+        String[] warningMessages = CommonUtils.EMPTY_STRING_ARRAY;
+        verifyManifestFile(testFileName, warningMessages);
+    }
+
+    public void verifyManifestFile(String fileName, String[] warningMessages) throws Exception {
+        String filePath = getPath(TEST_DIRECTORY_NAME + File.separator + fileName);
+        verify(configuration, filePath, warningMessages);
+    }
+
+    @Override
+    protected DefaultConfiguration createCheckerConfig(Configuration config) {
+        DefaultConfiguration dc = new DefaultConfiguration("root");
+        dc.addChild(config);
+        return dc;
+    }
+
+}
