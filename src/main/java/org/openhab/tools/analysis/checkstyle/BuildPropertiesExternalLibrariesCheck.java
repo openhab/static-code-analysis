f8d1f24763d27eaac2f3b35b26a39d86dde6b693
VelinYordanov <velin.iordanov@gmail.com>
2017-08-22 11:02:05 +0300

    Initial contribution to the external libraries checks
    -ManifestExternalLibrariesCheck
    -BuildPropertiesExternalLibrariesCheck
    -OutsideOfLibExternalLibrariesCheck
    
    Closes #105
    
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

---
+++ src/main/java/org/openhab/tools/analysis/checkstyle/BuildPropertiesExternalLibrariesCheck.java
@@ -0,0 +1,126 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.tools.analysis.checkstyle;
+
+import static org.openhab.tools.analysis.checkstyle.api.CheckConstants.PROPERTIES_EXTENSION;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.pde.core.build.IBuildEntry;
+import org.openhab.tools.analysis.checkstyle.api.AbstractExternalLibrariesCheck;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
+import com.puppycrawl.tools.checkstyle.api.FileText;
+
+/**
+ * A check that asserts that all jar files in the lib folder are added to build.properties and all jar files in
+ * build.properties
+ * are added to the lib folder.
+ *
+ * @author Velin Yordanov - Initial contribution
+ *
+ */
+public class BuildPropertiesExternalLibrariesCheck extends AbstractExternalLibrariesCheck {
+    private static final String BIN_INCLUDES = "bin.includes";
+    private static final String FILES_NEED_TO_BE_IN_A_LIB_FOLDER = "All jar files need to be placed inside a lib folder.";
+    private static final String JAR_PRESENT_IN_BUILD_PROPERTIES_NOT_IN_LIB = "The file %s is present in the build properties but not in the lib folder.";
+    private static final String JAR_PRESENT_IN_LIB_NOT_IN_BUILD_PROPERTIES = "The jar file %s is present in the lib folder but is not present in the build properties";
+    private static final Logger logger = LoggerFactory.getLogger(BuildPropertiesExternalLibrariesCheck.class);
+
+    public BuildPropertiesExternalLibrariesCheck() {
+        // build.properties will not be explicitly processed by the check.
+        setFileExtensions(PROPERTIES_EXTENSION);
+    }
+
+    private List<String> getBuildPropertiesJarFiles(File buildProperties) {
+        List<String> buildPropertiesJarFiles = new ArrayList<>();
+        try {
+            for (String token : getBinIncludesTokens(buildProperties)) {
+                if (token.contains(JAR_FILE_EXTENSION)) {
+                    // Trimming in case there is a space at the end of the entry
+                    buildPropertiesJarFiles.add(token.trim());
+                }
+            }
+        } catch (IOException e) {
+            logger.error(COULD_NOT_OPEN_BUILD_PROPERTIES);
+        }
+
+        return buildPropertiesJarFiles;
+    }
+
+    private String[] getBinIncludesTokens(File buildProperties) throws IOException {
+        IBuildEntry binIncludes = null;
+        try {
+            binIncludes = parseBuildProperties(buildProperties).getEntry(BIN_INCLUDES);
+        } catch (CheckstyleException ex) {
+            throw new IOException(COULD_NOT_OPEN_BUILD_PROPERTIES);
+        }
+
+        return binIncludes.getTokens();
+    }
+
+    private boolean checkIfBuildPropertiesContainsFolder(String[] binIncludesTokens) {
+        List<String> tokens = Arrays.asList(binIncludesTokens);
+
+        return (tokens.contains(LIB_FOLDER_NAME + "/") || tokens.contains(LIB_FOLDER_NAME + "/*"));
+    }
+
+    private boolean checkIfLibFolderExists(File file) {
+        File rootFolder = file.getParentFile();
+        for (File bundleFile : rootFolder.listFiles()) {
+            if (LIB_FOLDER_NAME.equals(bundleFile.getName())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    protected void processFiltered(File file, FileText fileText) throws CheckstyleException {
+        final String rootFolderPath = file.getParentFile().getAbsolutePath();
+        final File libDirectory = new File(rootFolderPath + File.separator + LIB_FOLDER_NAME);
+        String[] binIncludes = null;
+        try {
+            binIncludes = getBinIncludesTokens(file);
+        } catch (IOException e) {
+            logger.error(COULD_NOT_OPEN_BUILD_PROPERTIES);
+            return;
+        }
+
+        boolean containsFolder = checkIfBuildPropertiesContainsFolder(binIncludes);
+        boolean containsLib = checkIfLibFolderExists(file);
+
+        List<String> libJarFiles = new ArrayList<>();
+        if (containsLib) {
+            libJarFiles = getLibFolderJarFiles(libDirectory, file.getAbsolutePath());
+        }
+
+        if (containsFolder && containsLib) {
+            return;
+        }
+
+        List<String> buildPropertiesJarFiles = getBuildPropertiesJarFiles(file);
+
+        if (containsFolder || !buildPropertiesJarFiles.isEmpty()) {
+            if (!containsLib) {
+                log(0, FILES_NEED_TO_BE_IN_A_LIB_FOLDER);
+                return;
+            }
+        }
+
+        checkFiles(buildPropertiesJarFiles, libJarFiles, JAR_PRESENT_IN_BUILD_PROPERTIES_NOT_IN_LIB);
+        checkFiles(libJarFiles, buildPropertiesJarFiles, JAR_PRESENT_IN_LIB_NOT_IN_BUILD_PROPERTIES);
+    }
+}
---
+++ src/main/java/org/openhab/tools/analysis/checkstyle/OutsideOfLibExternalLibrariesCheck.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.tools.analysis.checkstyle;
+
+import static org.openhab.tools.analysis.checkstyle.api.CheckConstants.PROPERTIES_EXTENSION;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+import org.openhab.tools.analysis.checkstyle.api.AbstractExternalLibrariesCheck;
+
+import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
+import com.puppycrawl.tools.checkstyle.api.FileText;
+
+/**
+ * Verifies that all jar files in the bundle are located in a folder named "lib".
+ *
+ * @author Velin Yordanov - Initial contribution
+ *
+ */
+public class OutsideOfLibExternalLibrariesCheck extends AbstractExternalLibrariesCheck {
+    private static final String TARGET_FOLDER = "target";
+    private static final String JAR_FILES_NEED_TO_BE_PLACED_IN_A_LIB_FOLDER = "All jar files need to be placed inside a lib folder and added to MANIFEST.MF and build.properties";
+
+    public OutsideOfLibExternalLibrariesCheck() {
+        setFileExtensions(PROPERTIES_EXTENSION);
+    }
+
+    private void checkBundleForOutOfPlaceJarFiles(File bundleDirectory, String folderToSkip) {
+        File[] files = bundleDirectory.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                // When build with maven a "target" folder is generated.
+                if (name.equals(folderToSkip) || name.equals(TARGET_FOLDER)) {
+                    return false;
+                }
+
+                if (name.endsWith(JAR_FILE_EXTENSION)) {
+                    log(0, JAR_FILES_NEED_TO_BE_PLACED_IN_A_LIB_FOLDER);
+                }
+
+                return dir.isDirectory();
+            }
+        });
+
+        if (files != null) {
+            for (File file : files) {
+                checkBundleForOutOfPlaceJarFiles(file, null);
+            }
+        }
+    }
+
+    @Override
+    protected void processFiltered(File file, FileText fileText) throws CheckstyleException {
+        File bundleDirectory = file.getParentFile();
+        checkBundleForOutOfPlaceJarFiles(bundleDirectory, LIB_FOLDER_NAME);
+    }
+}
