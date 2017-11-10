3406e40c388f23530a307f31fc23accd192c4559
Svilen Valkanov <svilen.valkanov@musala.com>
2017-11-10 17:40:04 +0200

    Split project into modules
    Project split into individual modules for:
    - the Maven plugin
    - Checkstyle custom checks
    - PMD Custom Checks
    - FindBugs Custom checks
    
    After the change the custom checks will be deployed separately
    from the Maven plugin.
    
    Additionally:
    - Reduced scope of some dependencies to "provided";
    - Removed some incorrect imports;
    - Removed some transitive dependencies (commons-io, commons-lang,..).
    
    Signed-off-by: Svilen Valkanov <svilen.valkanov@musala.com>

---
+++ custom-checks/pmd/src/test/java/org/openhab/tools/analysis/pmd/test/ClasspathTest.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.tools.analysis.pmd.test;
+
+import org.junit.Before;
+
+import net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+
+/**
+ * Test class that includes all custom PMD tests for the .classpath files
+ *
+ * @author Svilen Valkanov
+ */
+
+public class ClasspathTest extends SimpleAggregatorTst {
+
+    // How to implement PMD rule test - https://pmd.github.io/pmd-5.4.1/customizing/rule-guidelines.html
+    @Override
+    @Before
+    protected void setUp() {
+        addRule("src/test/resources/pmd/ruleset/classpath.xml", "AvoidMavenPomderivedInClasspath");
+    }
+}
---
+++ custom-checks/pmd/src/test/java/org/openhab/tools/analysis/pmd/test/PomTest.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.tools.analysis.pmd.test;
+
+import org.junit.Before;
+
+import net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+
+/**
+ * Test class that includes all custom PMD tests for the pom.xml files
+ *
+ * @author Svilen Valkanov
+ *
+ */
+public class PomTest extends SimpleAggregatorTst {
+
+    @Override
+    @Before
+    protected void setUp() {
+        addRule("src/test/resources/pmd/ruleset/pom.xml", "AvoidOverridingParentPomConfiguration");
+    }
+}
