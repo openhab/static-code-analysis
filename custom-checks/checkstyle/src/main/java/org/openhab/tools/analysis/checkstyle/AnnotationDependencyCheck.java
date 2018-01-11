a96f51ba38f391c7692058c3529d48221381dbb8
Kristina S. Simova <kssimovaa@gmail.com>
2018-01-11 11:19:52 +0200

    Added check to generate WARNING if a bundle does not have optional Import-Package dependency to org.eclipse.jdt.annotation
    Signed-off-by: Kristina S. Simova <kssimovaa@gmail.com>

---
+++ custom-checks/checkstyle/src/main/java/org/openhab/tools/analysis/checkstyle/AnnotationDependencyCheck.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2010-2018 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.tools.analysis.checkstyle;
+
+import static org.openhab.tools.analysis.checkstyle.api.CheckConstants.MANIFEST_EXTENSION;
+
+import java.io.File;
+import java.util.Set;
+
+import org.apache.ivy.osgi.core.BundleInfo;
+import org.apache.ivy.osgi.core.BundleRequirement;
+import org.openhab.tools.analysis.checkstyle.api.AbstractStaticCheck;
+
+import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
+import com.puppycrawl.tools.checkstyle.api.FileText;
+
+/**
+ * Checks if every bundle has optional Import-Package dependency to org.eclipse.jdt.annotation and generates a <b>
+ * WARNING </b> if it is missing.
+ *
+ * @author Kristina Simova - Initial contribution
+ *
+ */
+public class AnnotationDependencyCheck extends AbstractStaticCheck {
+    private static final String DEPENDENCY_NAME = "org.eclipse.jdt.annotation";
+    private static final String DEPENDENCY_RESOLUTION = "optional";
+    private static final String IMPORT_PACKAGE_HEADER_NAME = "Import-Package";
+    private static final String WARNING_MESSAGE_MISSING_DEPENDENCY_RESOLUTION = "Every bundle should have optional Import-Package dependency to org.eclipse.jdt.annotation.";
+
+    public AnnotationDependencyCheck() {
+        setFileExtensions(MANIFEST_EXTENSION);
+    }
+
+    @Override
+    protected void processFiltered(File file, FileText fileText) throws CheckstyleException {
+        if (isEmpty(fileText)) {
+            // it should be handled by another check
+            return;
+        }
+        BundleInfo bundleInfo = parseManifestFromFile(fileText);
+        Set<BundleRequirement> bundleRequirements = bundleInfo.getRequirements();
+        int lineNo = findLineNumberSafe(fileText, IMPORT_PACKAGE_HEADER_NAME, 0,
+                "Could not find Import-Package dependency!");
+        if (lineNo == 0) {
+            // it means that the MANIFEST.MF file has no Import-Package dependency
+            return;
+        }
+        boolean hasOptionalDependencyResolution = false;
+        for (BundleRequirement requirement : bundleRequirements) {
+            String dependencyName = requirement.getName();
+            String dependencyResolution = requirement.getResolution();
+            if (DEPENDENCY_NAME.equals(dependencyName) && DEPENDENCY_RESOLUTION.equals(dependencyResolution)) {
+                hasOptionalDependencyResolution = true;
+                break;
+            }
+        }
+        if (!hasOptionalDependencyResolution) {
+            log(lineNo, WARNING_MESSAGE_MISSING_DEPENDENCY_RESOLUTION);
+        }
+    }
+
+}
