755071c7289914389432e430098a58ae89c9ddb8
Kristina S. Simova <kssimovaa@gmail.com>
2018-01-22 14:58:15 +0200

    Changed parsing library to flexmark
    Signed-off-by: Kristina S. Simova <kssimovaa@gmail.com>

--- custom-checks/checkstyle/src/main/java/org/openhab/tools/analysis/checkstyle/readme/MarkdownVisitor.java
+++ custom-checks/checkstyle/src/main/java/org/openhab/tools/analysis/checkstyle/readme/MarkdownVisitor.java
@@ -8,34 +8,28 @@
  */
 package org.openhab.tools.analysis.checkstyle.readme;
 
-import java.text.MessageFormat;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.commons.lang.StringUtils;
-import org.commonmark.node.AbstractVisitor;
-import org.commonmark.node.BulletList;
-import org.commonmark.node.Code;
-import org.commonmark.node.FencedCodeBlock;
-import org.commonmark.node.Heading;
-import org.commonmark.node.IndentedCodeBlock;
-import org.commonmark.node.ListBlock;
-import org.commonmark.node.Node;
-import org.commonmark.node.OrderedList;
-import org.commonmark.node.Paragraph;
-import org.commonmark.node.Text;
 import org.openhab.tools.analysis.checkstyle.api.AbstractStaticCheck;
-import org.openhab.tools.analysis.checkstyle.api.NoResultException;
 
 import com.puppycrawl.tools.checkstyle.api.FileText;
+import com.vladsch.flexmark.ast.BulletList;
+import com.vladsch.flexmark.ast.FencedCodeBlock;
+import com.vladsch.flexmark.ast.Heading;
+import com.vladsch.flexmark.ast.ListBlock;
+import com.vladsch.flexmark.ast.ListItem;
+import com.vladsch.flexmark.ast.Node;
+import com.vladsch.flexmark.ast.NodeVisitorBase;
+import com.vladsch.flexmark.ast.OrderedList;
+import com.vladsch.flexmark.ast.Paragraph;
 
 /**
  * This visitor processes headers, lists and code sections and logs errors when
  * needed.
  *
  * @author Erdoan Hadzhiyusein - Initial contribution
+ * @author Lyubomir Papazov - Change the parsering library to flexmark and adjust the code to work with it
  */
-class MarkdownVisitor extends AbstractVisitor {
+class MarkdownVisitor extends NodeVisitorBase {
 
     private static final String EMPTY_LINE_AFTER_HEADER_MSG = "Missing an empty line after the Markdown header ('#').";
     private static final String EMPTY_LINE_BEFORE_LIST_MSG = "The line before a Markdown list must be empty.";
@@ -44,14 +38,8 @@ class MarkdownVisitor extends AbstractVisitor {
     private static final String EMPTY_LINE_AFTER_CODE_MSG = "The line after code formatting section must be empty.";
     private static final String EMPTY_CODE_BLOCK_WARNING = "There is an empty or unclosed code formatting section. Please correct it.";
     private static final String HEADER_AT_END_OF_FILE = "There is a header at the end of the Markdown file. Please consider adding some content below.";
-    private final Log logger = LogFactory.getLog(MarkdownVisitor.class);
 
-    /**
-     * This field stores the line number where the processing of the source is up to.
-     * Every time a MarkDown element is visited the pointer stores its starting line number in the source.
-     * And when it is processed the pointer stores its ending line.
-     */
-    private int currentLinePointer = 0;
+    private static final String REGEX_NEW_LINES = "\\\r?\\\n";
 
     /**
      * A callback is used in order to use the protected methods of {@link AbstractStaticCheck}
@@ -68,41 +56,23 @@ class MarkdownVisitor extends AbstractVisitor {
     /**
      * Example of heading: #HomeMatic Binding
      */
-    @Override
     public void visit(Heading heading) {
-        String headerContent = getLiteralOfElement(heading);
-        validateHeader(headerContent);
+        validateHeadingPosition(heading.getLineNumber());
     }
 
-    /**
-     * This method processes the Headings in the README which the Markdown
-     * parser returns.
-     *
-     * @param headerValue - a String containing the literal of found Heading node
-     */
-    private void validateHeader(String headerValue) {
-        if (headerValue != null) {
-            int headerLineNumber;
-            try {
-                headerLineNumber = callback.findLineNumber(fileText, headerValue, currentLinePointer);
-                currentLinePointer = headerLineNumber;
-                boolean isHeaderAtEndOfFile = headerLineNumber == fileText.size();
-                if (isHeaderAtEndOfFile) {
-                    callback.log(headerLineNumber, HEADER_AT_END_OF_FILE);
-                } else {
-                    boolean isNextLineEmpty = StringUtils.isBlank(fileText.get(headerLineNumber));
-                    if (!isNextLineEmpty) {
-                        callback.log(headerLineNumber, EMPTY_LINE_AFTER_HEADER_MSG);
-                    }
-                }
-            } catch (NoResultException e) {
-                logger.error("A header cannot be processed properly: " + headerValue, e);
-            }
+    private void validateHeadingPosition(int zeroBasedHeaderLineNumber) {
+
+        boolean isHeaderAtEndOfFile = zeroBasedHeaderLineNumber == fileText.size() - 1;
+        if (isHeaderAtEndOfFile) {
+            // log the one=based line number
+            callback.log(zeroBasedHeaderLineNumber, HEADER_AT_END_OF_FILE);
         } else {
-            String message = MessageFormat.format(
-                    "Occurred an error while processing the Markdown file {0}. The header value is null.",
-                    fileText.getFile().getAbsolutePath());
-            logger.warn(message);
+            // FileText uses zero-based indexes
+            boolean isNextLineEmpty = StringUtils.isBlank(fileText.get(zeroBasedHeaderLineNumber + 1));
+            if (!isNextLineEmpty) {
+                // log the one=based line number
+                callback.log(zeroBasedHeaderLineNumber, EMPTY_LINE_AFTER_HEADER_MSG);
+            }
         }
     }
 
@@ -110,64 +80,33 @@ class MarkdownVisitor extends AbstractVisitor {
      * A fencedCodeBlock is a text-formatted like a programming code, example:
      * <code>private String name=null;</code>
      */
-    @Override
     public void visit(FencedCodeBlock fencedCodeBlock) {
-        String codeLiteral = fencedCodeBlock.getLiteral();
-        validateCodeSection(codeLiteral);
+        validateCodeSectionPosition(fencedCodeBlock.getLineNumber(), fencedCodeBlock.getEndLineNumber(),
+                fencedCodeBlock);
     }
 
-    /**
-     * Processes FencedCodeBlock nodes the parser returns.
-     *
-     * @param codeBlock - multiLine String containing the literal of the code block
-     */
-    private void validateCodeSection(String codeBlock) {
-        int codeStartingLineNumber = 0;
-        int codeEndingLineNumber = 0;
+    private void validateCodeSectionPosition(int zeroBasedStartLineNumber, int zeroBasedEndLineNumber,
+            Node codeBlockText) {
 
-        // Splitting the String in case it is multiline, else the array would
-        // have only one element
-        String codeSectionLines[] = codeBlock.split("\\r?\\n");
-        if (!StringUtils.isBlank(codeBlock)) {
-            try {
-                codeStartingLineNumber = callback.findLineNumber(fileText, codeSectionLines[0], currentLinePointer);
-                codeStartingLineNumber--;
-                currentLinePointer = codeStartingLineNumber;
-                // Start from the line above the code section
-                verifyBeforeCodeSection(codeStartingLineNumber);
-                codeEndingLineNumber = codeStartingLineNumber + codeSectionLines.length;
-                codeEndingLineNumber++;
-                currentLinePointer = codeEndingLineNumber;
-                verifyAfterCodeSection(codeEndingLineNumber);
-            } catch (NoResultException e) {
-                logger.error("A code section wasn't processed properly! " + codeBlock, e);
+        Node codeSection = codeBlockText.getFirstChild();
+
+        // Check if the code section is empty or blank
+        if (codeSection != null && !StringUtils.isBlank(codeSection.getChars().toString())) {
+
+            // The code block is not the first line, and the previous line is not empty
+            if (zeroBasedStartLineNumber == 0 || !StringUtils.isBlank(fileText.get(zeroBasedStartLineNumber - 1))) {
+                // log the one-based line number
+                callback.log(zeroBasedStartLineNumber, EMPTY_LINE_BEFORE_CODE_MSG);
+            }
+
+            if (zeroBasedEndLineNumber != fileText.size() - 1
+                    && !StringUtils.isBlank(fileText.get(zeroBasedEndLineNumber + 1))) {
+                // log the one-based line number
+                callback.log(zeroBasedEndLineNumber, EMPTY_LINE_AFTER_CODE_MSG);
             }
         } else {
-            callback.log(currentLinePointer, EMPTY_CODE_BLOCK_WARNING);
-        }
-    }
-
-    private void verifyBeforeCodeSection(int codeStartingLineNumber) {
-        if (codeStartingLineNumber == 1) {
-            callback.log(codeStartingLineNumber, EMPTY_LINE_BEFORE_CODE_MSG);
-        } else {
-            // -2 because the line before the code is occupied with code section opening- ```
-            int lineBeforeCodeSection = codeStartingLineNumber - 2;
-            boolean isLineBeforeCodeSectionEmpty = StringUtils.isBlank(fileText.get(lineBeforeCodeSection));
-            if (!isLineBeforeCodeSectionEmpty) {
-                callback.log(codeStartingLineNumber, EMPTY_LINE_BEFORE_CODE_MSG);
-            }
-        }
-    }
-
-    private void verifyAfterCodeSection(int codeEndingLineNumber) {
-        boolean isCodeSectionAtEndOfFile = (codeEndingLineNumber == fileText.size());
-        // There is another check that logs errors if there is not an empty line at the end of file
-        // named NewLineAtEndOfFileCheck
-        if (!isCodeSectionAtEndOfFile) {
-            if (!StringUtils.isBlank(fileText.get(codeEndingLineNumber))) {
-                callback.log(codeEndingLineNumber, EMPTY_LINE_AFTER_CODE_MSG);
-            }
+            // log the one-based line number
+            callback.log(zeroBasedStartLineNumber, EMPTY_CODE_BLOCK_WARNING);
         }
     }
 
@@ -177,162 +116,56 @@ class MarkdownVisitor extends AbstractVisitor {
      * @param listBlock - a block type which is common parent of {@link BulletList } and {@link OrderedList}
      */
     private void processListBlock(ListBlock listBlock) {
-        String firstLineOfList = getFirstLineInList(listBlock);
-        String lastLineOfList = getLastLineInList(listBlock);
-        if (lastLineOfList == null) {
-            // in case it is one lined list (last line would be null)
-            lastLineOfList = firstLineOfList;
-        }
-        int listLenght = getListLenght(listBlock);
-        markDownListProcessing(firstLineOfList, lastLineOfList, listLenght);
+        checkEmptyLineBefore(listBlock);
+        checkEmptyLineAfterList(listBlock);
     }
 
-    private int getListLenght(ListBlock listBlock) {
-        MarkdownListVisitor listVisitor = new MarkdownListVisitor();
-        listBlock.accept(listVisitor);
-        return listVisitor.getListLenght();
+    private void checkEmptyLineBefore(ListBlock listBlock) {
+        int firstLineOfList = listBlock.getLineNumber();
+
+        boolean isInnerList = listBlock.getParent() instanceof ListItem;
+        if (isInnerList) {
+            // Not checking if the line above is empty if it's another list item
+            return;
+        } else {
+            boolean isListfirstLineInFile = firstLineOfList == 0;
+            // The first line of the file can NOT be list
+            if (isListfirstLineInFile || !StringUtils.isBlank(fileText.get(firstLineOfList - 1))) {
+                // Log the one-based first line of the list
+                callback.log(firstLineOfList, EMPTY_LINE_BEFORE_LIST_MSG);
+            }
+        }
+    }
+
+    private void checkEmptyLineAfterList(ListBlock listBlock) {
+        ListItem lastListItem = (ListItem) listBlock.getLastChild();
+        Node lastListItemContent = lastListItem.getLastChild();
+
+        boolean isListEnd = lastListItemContent instanceof Paragraph;
+        if (isListEnd) {
+            String[] lastListItemlines = lastListItemContent.getChars().toString().split(REGEX_NEW_LINES);
+            if (lastListItemlines.length > 1) {
+                // Log the one-based line where there is an empty line
+                callback.log(lastListItemContent.getLineNumber(), EMPTY_LINE_AFTER_LIST_MSG);
+            }
+        }
+    }
+
+    public void visit(ListBlock list) {
+        list.getChildIterator().forEachRemaining(listItem -> visit(listItem));
+        processListBlock(list);
     }
 
     @Override
-    public void visit(BulletList bulletList) {
-        processListBlock(bulletList);
-    }
-
-    @Override
-    public void visit(OrderedList orderedList) {
-        processListBlock(orderedList);
-    }
-
-    /**
-     * This method processes list blocks in the specified Markdown file.
-     *
-     * @param firstLineOfList - the literal of the first list item
-     * @param lastLineOfList - the literal of the last list item
-     * @param listLenght - the number of list items (note that they can be multiLine)
-     */
-    private void markDownListProcessing(String firstLineOfList, String lastLineOfList, int listLenght) {
-        int listStartingLineNumber = 0;
-        int listEndingLineNumber = 0;
-        try {
-            listStartingLineNumber = callback.findLineNumber(fileText, firstLineOfList, currentLinePointer);
-        } catch (NoResultException e) {
-            logger.error("A list starting cannot be processed properly: " + firstLineOfList, e);
-        }
-        if (listStartingLineNumber == 1) {
-            callback.log(listStartingLineNumber, EMPTY_LINE_BEFORE_LIST_MSG);
+    protected void visit(Node node) {
+        if (node instanceof FencedCodeBlock) {
+            visit((FencedCodeBlock) node);
+        } else if (node instanceof Heading) {
+            visit((Heading) node);
+        } else if (node instanceof ListBlock) {
+            visit((ListBlock) node);
         } else {
-            // pointer goes before the list ending to narrow the searching scope
-            // sometimes there could be 2 list items with same literal
-            currentLinePointer = listStartingLineNumber + listLenght - 2;
-            try {
-                listEndingLineNumber = callback.findLineNumber(fileText, lastLineOfList, currentLinePointer);
-                currentLinePointer = listEndingLineNumber;
-                verifyLineBeforeListBlock(listStartingLineNumber);
-                verifyLineAfterListBlock(listEndingLineNumber);
-            } catch (NoResultException e) {
-                logger.error("A list ending cannot be processed properly: " + lastLineOfList, e);
-            }
+            visitChildren(node);
         }
-
-    }
-
-    private void verifyLineBeforeListBlock(int listStartingLineNumber) {
-        // Starting number is decreased with 2 lines because previous line is get and there is 0 indexation
-        boolean isPreviousLineEmpty = !StringUtils.isBlank(fileText.get(listStartingLineNumber - 2));
-        if (isPreviousLineEmpty) {
-            // the -1 is used when logging the error to mark the exact line which have to be empty
-            callback.log(listStartingLineNumber - 1, EMPTY_LINE_BEFORE_LIST_MSG);
-        }
-    }
-
-    private void verifyLineAfterListBlock(int listEndingLineNumber) {
-        boolean isListAtEndOfFile = (listEndingLineNumber == fileText.size());
-        if (!isListAtEndOfFile) {
-            boolean isNextLineEmpty = StringUtils.isBlank(fileText.get(listEndingLineNumber));
-            if (!isNextLineEmpty) {
-                callback.log(listEndingLineNumber, EMPTY_LINE_AFTER_LIST_MSG);
-            }
-        }
-        // If the list block is the last entry in the markdown file, the check for an empty
-        // line after the list will not be performed in order to avoid reporting false positives.
-    }
-
-    /**
-     * If there is a specific type of header or list item this method recursively gets its literal
-     *
-     * @param node - The node that is being processed to get its literal
-     * @return - returns null if the literal wasn't processed properly
-     */
-    private String getLiteralOfElement(Node node) {
-        if (node != null) {
-            if (node instanceof Text) {
-                Text text = (Text) node;
-                return text.getLiteral();
-            } else if (node instanceof Code) {
-                // return the literal of a code-formatted list items
-                Code code = (Code) node;
-                return code.getLiteral();
-            } else if (node instanceof IndentedCodeBlock) {
-                // In case there is a multiline code block as a child of list item
-                IndentedCodeBlock code = (IndentedCodeBlock) node;
-                String codeSectionLines[] = code.getLiteral().split("(\\r?\\n)|(\\r)");
-                if (codeSectionLines.length != 0) {
-                    return codeSectionLines[codeSectionLines.length - 1];
-                }
-            } else {
-
-                // The children are processed recursively till text or code node is found
-                // First child is used because Text and Code nodes are always first child nodes
-                return getLiteralOfElement(node.getFirstChild());
-            }
-        }
-        return null;
-    }
-
-    /**
-     * The first child represents the first list item in the list block. Then recursively check if there are
-     * any child nodes of this item and get the literal of it. Recursion is needed because there could be lists with
-     * different structure and formatting.
-     *
-     * @param node - the listblock which first line is wanted
-     * @return - returns the literal of the first line in the list
-     **/
-    private String getFirstLineInList(Node node) {
-        Node firstChildNode = node.getFirstChild();
-        // A paragraph is always the first parent of the leaf node
-        boolean isLeafListItem = node instanceof Paragraph;
-        String literalOfFirstChild = null;
-        if (firstChildNode != null && !isLeafListItem) {
-            // recursively searching the first list item and its nested lists
-            literalOfFirstChild = getFirstLineInList(firstChildNode);
-        } else {
-            literalOfFirstChild = getLiteralOfElement(node);
-        }
-        return literalOfFirstChild;
-    }
-
-    /**
-     * The last child represents the last list item in the list block. Then recursively check if there are
-     * any child nodes of this item and get the literal of it. Recursion is needed because there could be lists with
-     * different structure and formatting.
-     *
-     * @param node - the listblock of which last line is wanted
-     * @return - returns the literal of the last line in the list
-     **/
-    private String getLastLineInList(Node node) {
-        Node lastChildNode = node.getLastChild();
-        boolean isLeafListItem = node instanceof Paragraph;
-        String literalOfLastChild = null;
-        if (lastChildNode != null && !isLeafListItem) {
-            // recursively searching the last list item and its nested lists
-            // if there is a nested code block its last line is taken
-            literalOfLastChild = getLastLineInList(lastChildNode);
-
-        } else {
-            // this method is used to get the literal of leaf items or items which
-            // could be nested inside typical leaf items
-            literalOfLastChild = getLiteralOfElement(node);
-        }
-        return literalOfLastChild;
     }
 }
---
+++ custom-checks/checkstyle/src/test/resources/checkstyle/markdownCheckTest/org.openhab.binding.exec/README.md
@@ -0,0 +1,144 @@
+# Exec Binding
+
+This binding integrates the possibility to execute arbitrary shell commands.
+
+## Supported Things
+
+Currently, the binding supports a single type of Thing, being the `command` Thing.
+
+## Binding Configuration
+
+The binding does not require any specific configuration.
+
+Note that on Unix systems the commands are executed in the context and with the privileges of the process running the java virtual machine, and it is not advised to run the virtual machine as superuser/root. It is advised to test the execution of the command in a shell using the user owning the JVM process, for example
+
+```
+sudo -u openhab <command>
+```
+
+The execution of the commands is triggered by either sending a Command or State Update to the input Channel of the `command` Thing, if configured so, or by sending the ON Command to the run Channel of the `command` Thing (see below)
+
+## Thing Configuration
+
+The `command` Thing requires the following mandatory configuration parameters:
+
+- [command] the command to execute on the shell. 
+
+Optionally one can specify:
+
+- [transform] a transformation to apply on the execution result, 
+- [interval] an interval, in seconds, the [command] will be repeatedly executed, 
+- [timeout] a time-out, in seconds, after which the execution of the [command] will time out,
+- [runOnInput] a boolean parameter to make the [command] execute immediately every time a Command or a State Update is sent to the input channel of the `command` Thing, and lastly, 
+- [repeatEnabled] a boolean parameter to allow the [command] to be executed repeatedly, e.g. when the same Command or State Update is sent to input channel repeatedly. This parameter only makes sense when used in combination with [runOnInput] set to true.
+
+For each [command] a separate Thing has to be defined. For example,
+
+```
+Thing exec:command:apc [command="/usr/local/bin/apcaccess  status", interval=15, timeout=5, runOnInput=true, repeatEnabled=true]
+```
+
+[command] itself can be enhanced using some specific qualifiers that will be substituted by actual values at runtime. The qualifiers have the following syntax:
+
+`${<key>:<transform>:<formatter>}`
+
+whereby 
+
+- <key> can be either
+     - the name of an Item
+     - `exec-input`, denoting the current State of the input Channel of the `command` Thing
+     - `exec-time`, denoting the current date (as java.util.Date)     
+- <transform> is any valid [Transformation](https://docs.openhab.org/addons/transformations.html) service expression, e.g. REGEX((.*?)). <transform> is mandatory for the `exec-time` key
+- <formatter> is a formatting string using the well known syntax of the [java.util.Formatter](http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax) class
+
+At runtime the binding will grab the value of the <key>, transform it with the <transform> expression, and finally format the transformation result using the <formatter> format. For example,
+
+```
+Thing exec:command:lightcontroller [command="/usr/local/bin/light.sh  ${lightSwitch:MAP(en.map):%1$s}"]
+```
+
+When the [command] of the lightcontroller item is executed then the value of the lightSwitch Item is retrieved, transformed using the en.map MAP Transform, and passed on as a literal string (i.e. %1$s takes the first argument of the result of the Transform, and formats it as a String) to the ligh.sh script
+
+Nesting of substitution keys is supported, e.g. `${lightSwitch_${lightCounter}}` will resolve the lightCounter Item first, and then subsequently resolve the second substitution key. For example, if the value of lighCounter would happen to be 3, then the second substitution resolved to the value of Item `lighSwitch_3`
+
+```
+Thing exec:command:lightcontroller [command="/usr/local/bin/light.sh  ${exec-input}"]
+```
+
+In the above example, exec-input is substituted with the actual State of the input Channel of the `exec:command:lightcontroller` Thing
+
+The Channels themselves are defined as custom State Channels (https://github.com/eclipse/smarthome/blob/master/docs/documentation/features/dsl.md#defining-channels), so one can freely define the Type of each Channel. The `command` Thing supports both an `input` and `output` Channel to set an input for the [command] and get the output of the command execution.
+
+## Channels
+
+All Things support the following channels:
+
+| Channel Type ID | Item Type    | Description                               |
+|-----------------|--------------|-------------------------------------------|
+| input           | custom       | Input parameter to provide to the command |
+| output          | custom       | Output of the last execution of the command |
+| exit            | Number       | The exit value of the last execution of the command |
+| run             | Switch       | Send ON to execute the command. the current State of this channel tells whether the command is running or not |
+| lastexecution   | DateTime     | Time/Date the command was last executed, in yyyy-MM-dd'T'HH:mm:ss.SSSZ format |
+
+## Full Example
+
+Some additional examples can be found on the [OpenHAB community forum](https://community.openhab.org/t/1-openhab-433mhz-radio-transmitter-tutorial/34977)
+
+**demo.things**
+
+```
+Thing exec:command:switch_monitor [command="switch_control.sh check", interval=1]
+{
+Channels:
+    Switch : output
+}
+Thing exec:command:switch_control [command="switch_control.sh ${exec-input}", runOnInput=true, repeatEnabled=true] {
+Channels:
+        Switch : input
+        String : output
+}
+```
+
+**demo.items**
+
+```
+Switch LampSwitch {channel="exec:command:switch_control:input", channel="exec:command:switch_monitor:output"}
+String LampResult {channel="exec:command:switch_control:output"}
+Switch LampSwitching {channel="exec:comamnd:switch_control:run"}
+Number YourNumber "Your Number [%.1f �C]"
+```
+
+**demo.rules**
+
+```
+rule "Your Execution"
+  when
+     Item someTrigger changed
+  then
+        // set the additional command line arguments to the switch_control.sh script
+        if(YourTrigger.state == ON){
+                LampSwitch.sendCommand(ON)
+        }else{
+                LampSwitch.sendCommand(OFF)
+        }
+
+      // Trigger execution
+      LampSwitching.sendCommand(ON)
+
+      // wait for the command to complete
+      // State will be NULL if not used before or ON while command is executed
+      while(LampSwitching.state != OFF){
+         Thread::sleep(500)
+      }
+      
+      // Logging of command line result
+      logInfo("Switching the lamp", "Result:" + LampResult.state )
+      
+      // If the returned string is just a number it can be parsed
+      // If not a transformation service can be used
+      YourNumber.postUpdate(
+            (Integer::parseInt(LampResult.state.toString) as Number )
+      )
+end
+```
